e4f07dfc2b1879f9db38b3ad4e9fc218f263e134
Rodrigo Alexandre Vehman <rodvehman@hotmail.com>
2025-09-30 14:25:53 -0300

    Update

---
+++ fornecedores/listar.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/estilos.css">
+    <title>Listar Fornecedores</title>
+</head>
+<body>
+    <h1>Fornecedores</h1>
+    <a href="">Novo Fornecedor</a>
+    <a href="../index.php">Voltar</a>
+</body>
+</html>
--- index.php
+++ index.php
@@ -7,10 +7,11 @@
     <link rel="stylesheet" href="./css/estilos.css">
 </head>
 <body>
+    <div><img src="./images/flybynight-cover-ss.jpg" alt=""></div>
     <h1>Fly By Night - CRUD com PHP e SQL</h1>
     <h2>Gerenciar</h2>
     <ul>
-        <li><a href="">Fornecedores</a></li>
+        <li><a href="./fornecedores/listar.php">Fornecedores</a></li>
         <li><a href="">Produtos</a></li>
     </ul>
 </body>
