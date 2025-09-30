30a74f13f1a9c27e16588ff02816ef52f072ecbb
Rodrigo Alexandre Vehman <rodvehman@hotmail.com>
2025-09-30 17:09:10 -0300

    Update fornecedor_crud.php

--- src/fornecedor_crud.php
+++ src/fornecedor_crud.php
@@ -14,5 +14,15 @@
             return $consulta -> fetchAll();
     };
 
+    function inserirFornecedor($conexao, $nome){
+        // Montando comando SQL de INSERT e aplicando um "named parameter". Parâmetro nomeado é utilizado para atribuir um valor.
+        $sql = "INCLUDE INTO Fornecedores(nome) Values (:nome)";
+        // Prepare o comando acima antes de executar no BD.
+        $consulta = $conexao -> prepare($sql);
+        // Anexar/Atrelar um valor
+        $consulta -> bindValue(":nome",$nome);
+        // Executa a consulta com o comando e o valor
+        $consulta -> execute();
+    }
 
 ?>
