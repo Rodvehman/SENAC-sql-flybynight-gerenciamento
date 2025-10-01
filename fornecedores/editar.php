c2614af3c515a6a1b772f8935892ff21b02a7751
Rodrigo Alexandre Vehman <rodvehman@hotmail.com>
2025-10-01 15:31:12 -0300

    Update editar.php

--- fornecedores/editar.php
+++ fornecedores/editar.php
@@ -10,9 +10,7 @@
     // var_dump($fornecedor);
     // echo "</pre>";
 
-    
-    
-    
+      
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -25,9 +23,11 @@
 <body>
     <h1>Editar fornecedor</h1>
     <form action="" method="post">
+        <!-- Sempre coloque o código/id do registro de forma oculta no formulário -->
+        <input type="hidden" name="id" value="<?=$fornecedor['id']?>">
         <div>
             <label for="nome">Nome: </label>
-            <input type="text" name="nome" id="nome" required>
+            <input value="<?=$fornecedor['nome']?>" type="text" name="nome" id="nome" required>
         </div>
         <button type="submit">Atualizar</button>
     </form>
