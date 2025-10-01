25fb20a18442d3923f999ee881ba0aeb527eeaaa
Rodrigo Alexandre Vehman <rodvehman@hotmail.com>
2025-10-01 14:21:39 -0300

    Update

--- src/fornecedor_crud.php
+++ src/fornecedor_crud.php
@@ -1,3 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/estilos.css">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
 <?php
 /* Neste arquivo teremos todas as funções que serão usadas para manipulação de (CRUD) de Fornecedores em nosso sistema e banco de dados. */
 
