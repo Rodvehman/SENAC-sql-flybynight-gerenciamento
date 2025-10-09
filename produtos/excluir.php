4e63e65a86f05b20de1bc33543d0cdbaeb07eb95
Rodvehman <63985415+Rodvehman@users.noreply.github.com>
2025-10-09 18:19:19 -0300

    Update

--- produtos/excluir.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require_once '../src/produtos_crud.php';
-
-    $id = $_GET['id'];
-    excluirProdutos($conexao, $id);
-    header("location:listar.php");
-    exit;
-?>
--- src/produtos_crud.php
+++ src/produtos_crud.php
@@ -1,8 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/estilos.css">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+
 <?php
 
     require_once '../conecta.php';
 
     function buscarProdutos($conexao){
-        
+        $sql = "SELECT * FROM Produtos";
+        $consulta = $conexao -> query($sql);
+        return $consulta -> fetchAll();
     }
 ?>
