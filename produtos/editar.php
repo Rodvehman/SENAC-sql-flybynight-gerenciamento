35fd5b626876be3c0fcb902b9cd1803e32b6fa5b
Rodvehman <63985415+Rodvehman@users.noreply.github.com>
2025-10-14 14:05:36 -0300

    Update editar.php

--- produtos/editar.php
+++ produtos/editar.php
@@ -1,23 +1,3 @@
-<?php
-    require_once '../src/produtos_crud.php';
-
-    // Pegando da URL o parâmetro chamado id
-    $id = $_GET['id'];
-    // Chamamos a função, passando a conexão e o id do fornecedor a ser buscado
-    $fornecedor = buscarProdutosPorId($conexao, $id);
-    
-    // echo "<pre>";
-    // var_dump($fornecedor);
-    // echo "</pre>";
-    if ($_SERVER['REQUEST_METHOD'] === 'POST'){
-        $nome = $_POST['nome'];
-        atualizarProdutos($conexao, $nome, $id);
-        // Após direcionar usando header()...
-        header("location:listar.php");
-        // ... sempre encerre/interrompa o script (evitando erros/execuções adicionais)
-        exit;
-    }      
-?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -29,11 +9,18 @@
 <body>
     <h1>Editar Produtos</h1>
     <form action="" method="post">
-        <!-- Sempre coloque o código/id do registro de forma oculta no formulário -->
-        <input type="hidden" name="id" value="<?=$produto['id']?>">
         <div>
             <label for="nome">Nome: </label>
-            <input value="<?=$$produto['nome']?>" type="text" name="nome" id="nome" required>
+            <input type="text" name="nome" id="nome" required>
+            <label for="descricao">Descrição: </label>
+            <input type="text" name="descricao" id="descricao" required>
+            <label for="preco">Preço: </label>
+            <input type="float" name="preco" id="preco">
+            <label for="quantidade">Quantidade: </label>
+            <input type="int" name="quantidade" id="quantidade">
+            <label for="fornecedor_id">ID Fornecedor</label>
+            <input type="int" name="fornecedor_id" id="fornecedor_id">
+            
         </div>
         <button type="submit">Atualizar</button>
     </form>
