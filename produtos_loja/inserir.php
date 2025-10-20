4e8d52b0669353e1809d7b11e1380c69e8196c62
Rodvehman <63985415+Rodvehman@users.noreply.github.com>
2025-10-20 10:07:39 -0300

    Update

--- produtos_loja/inserir.php
+++ produtos_loja/inserir.php
@@ -1,17 +1,3 @@
-<?php
-    require_once "../src/lojas_produtos_crud.php";
-    require_once "../src/loja_crud.php";
-    require_once "../src/produtos_crud.php";
-
-    // if ($_SERVER['REQUEST_METHOD'] === 'POST'){
-    //     $loja = $_POST['loja'];
-    //     $produto = $_POST['produto'];
-    //     $quantidade = $_POST['quantidade'];
-    //     $loja_id = $_POST['loja_id'];
-    //     $produto_id = $_POST['produto_id'];
-
-    // }
-?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -21,33 +7,48 @@
     <title>Inserir Estoque</title>
 </head>
 <body>
+    <?php
+        require_once "../src/lojas_produtos_crud.php";
+        require_once "../src/loja_crud.php";
+        require_once "../src/produtos_crud.php";
+
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+            $loja_id = $_POST['loja_id'];
+            $produto_id = $_POST['produto_id'];
+            $estoque = $_POST['estoque'];
+            inserirEstoquePorLoja($conexao, $loja_id, $produto_id, $estoque);
+            header("location:listar.php");
+            exit;
+        }
+
+        $produtos_lojas = buscarProdutosPorLojas($conexao);
+    ?>
     <h1>Adicionando Estoque</h1>
     <a href="../produtos_loja/listar.php">🔙 Voltar</a>
     <form action="" method="post">
         <div>
-            <label for="">Loja:</label>
-                <select name="loja" type="text" id="loja">
+            <label for="loja">Loja:</label>
+                <select name="loja" type="text" id="loja" required>
                     <option value="--Selecione--"></option>
-                    <?php
-                        foreach ($produtos_lojas as $produto_loja): ?>
-                        <option value="<?=$produto_loja['loja_id']?>"<?=$produto_loja['loja_nome']?>></option>                     
-                        <?php endforeach ?>
+                    <?php foreach ($produtos_lojas as $produto_loja): ?>
+                    <option value="<?=$produto_loja['loja_id']?>"<?=$produto_loja['loja_nome']?>></option>                     
+                    <?php endforeach ?>
                 </select>
         </div>
         <div>
-            <label for="">Produto: </label>
-            <select value="produto" type="text" id="produto">
+            <label for="produto">Produto: </label>
+            <select value="produto" type="text" id="produto" required>
                 <option value="--Selecione--"></option>
-                <?php
-                    foreach ($produtos_lojas as $produto_loja): ?>
-                    <option value="<?=$produto_loja['produto_id']?>"<?=$produto_loja['produto_nome']?>></option>
-                    <?php endforeach ?>
+                <?php foreach ($produtos_lojas as $produto_loja): ?>
+                <option value="<?=$produto_loja['loja_id']?>"><?=$produto_loja['nome']?></option>
+                <?php endforeach ?>
             </select>
         </div>
         <div>
-            Quantidade:
-            <input type="int">
+            <label for="">Quantidade:</label>
+            <input class="quantidade" id="quantidade" type="int" required>
         </div>
+        <button type="submit">Enviar</button>
     </form>
 </body>
 </html>
